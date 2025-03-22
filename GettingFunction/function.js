af9a1ae8cae12ed5e525aaaaf7372903e53d9378
SELDJ <janjanarbiz@gmail.com>
2025-03-22 21:19:16 +0800

    Hello World

---
+++ GettingFunction/function.js
@@ -0,0 +1,73 @@
+function bark (name, weight) {
+    if (weight > 20) 
+        {console.log(name + ' says WOOF WOOF')
+} 
+else {
+    console.log (name + ' says woof woof')
+}
+
+}
+
+barking('pykke', 25)
+barking('Kerker', 34)
+barking('Jenny', 10)
+barking('Collar', 30)
+barking('Hercc', 41)
+
+
+var dogName = 'Jenny';
+var dogWeight = 10;
+
+    if (dogWeight > 20) {
+        console.log(dogName + ' says, WOOF WOOF')
+    }
+    else {
+        console.log(dogName + ' says, woof woof')
+    }
+
+
+// var dogName = 'Jenny';
+// var dogWeight = 7;
+
+//     if (dogWeight > 20) {
+//         console.log(dogName + ' says, WOOF WOOF')
+//     }
+//     else {
+//         console.log(dogName + ' says, woof woof')
+//     }
+
+// var dogName = 'Jenny';
+// var dogWeight = 12;
+
+//     if (dogWeight > 20) {
+//         console.log(dogName + ' says, WOOF WOOF')
+//     }
+//     else {
+//         console.log(dogName + ' says, woof woof')
+//     }
+
+// var dogName = 'Jenny';
+// var dogWeight = 23;
+
+//     if (dogWeight > 20) {
+//         console.log(dogName + ' says, WOOF WOOF')
+//     }
+//     else {
+//         console.log(dogName + ' says, woof woof')
+//     }
+    
+// var dogName = 'Pyke';
+// var dogWeight = 15;
+// bark(dogWeight, dogName);
+
+// var dogName = 'Collar';
+// var dogWeight = 24;
+// bark(dogWeight, dogName);
+
+// var dogName = 'KerKer';
+// var dogWeight = 30;
+// bark(dogWeight, dogName);
+
+// var dogName = 'Jenny';
+// var dogWeight = 10;
+// bark(dogWeight, dogName);
---
+++ battleship/battleship_v2.js
@@ -0,0 +1,52 @@
+console.log ('BattleShip Game');
+// Declaring Variables
+var randomLoc = Math.floor (Math.random() * 5);
+var location1 = randomLoc;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
+
+var guess;
+
+var hits = 0;
+var guesses = 0;
+
+var isSunk = false;
+
+// Game Logic
+while (isSunk == false) {
+    guess = prompt("Ready, Aim, Shoot! (Enter Number 0 - 6 🥳🫡)")
+
+        if (guess < 0 || guess > 6 ) {
+            alert("Please Enter Valid Number!")
+        }
+
+        if (guess == null) {
+            alert('Thank you for Playing! 💕')
+            break
+        }
+
+        else {
+            guesses += 1;
+            
+            if (guess == location1 || guess == location2 || guess == location3) {
+           
+                hits += 1;
+                alert("hit!🎯")
+            if (hits == 3) {
+                    
+                    isSunk = true;
+                    alert("You sank The Battleship! 🙄😒")
+
+                }
+            } else {
+                alert("miss! 😝")
+            }
+        }
+
+
+}
+
+var accuracy = (guesses > 0 ? hits/guesses * 100 : 0) .toFixed(2)
+var stats = "You sank my battleship at: " + guesses + " guesses" + " and you accuracy is: " + accuracy + '%';
+
+alert(stats);
